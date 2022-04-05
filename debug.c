ee8b0974f430ddffe5f7d8d17f2c03513725e2c0
lonequantum <lonequantum@lnode.net>
2022-04-05 18:34:18 +0200

    Display "mandatory" walls in another color on debug view

--- debug.c
+++ debug.c
@@ -12,6 +12,9 @@ void print_matrix(int size, int *lines[])
 		for (int column = 0; column < size; column++)
 			if (line == size / 2 && column == size / 2)
 				printf("><");
+			else if (line % 2 == 0 && column % 2 == 0
+			|| (line == 0 || line == size - 1 || column == 0 || column == size - 1) && lines[line][column] == 0)
+				printf("▒▒");
 			else if (lines[line][column])
 				printf("  ");
 			else
