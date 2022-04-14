83a4f563f41d208026edf037992d9c70713ca942
lonequantum <lonequantum@lnode.net>
2022-04-14 21:00:47 +0200

    Add glPolygonMode instructions

--- render.c
+++ render.c
@@ -105,6 +105,8 @@ bool prepare_scene(VERTICES_SET data)
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), NULL);
 	glEnableVertexAttribArray(0);
+	glPolygonMode(GL_BACK, GL_LINE);
+	glPolygonMode(GL_FRONT, GL_FILL);
 
 	// Closes what is no longer needed
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
