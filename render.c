bd8adde63062d58c49b779ccc8cc1cbcb3a10134
lonequantum <lonequantum@lnode.net>
2022-04-16 20:11:13 +0200

    Clear GL_DEPTH_BUFFER_BIT every frame

--- render.c
+++ render.c
@@ -124,7 +124,7 @@ bool prepare_scene(VERTICES_SET data)
 void draw_scene(void)
 {
 	glClearColor(0.1f, 0.1f, 0.3f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	static mat4 transform_matrix = {
 		1, 0, 0, 0,
